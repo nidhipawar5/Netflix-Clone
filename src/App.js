e74fdb175bb3b63b1bd5b29fd14847e03297c76c
nidhipawar5 <nidhipawar0504@gmail.com>
2020-09-25 16:18:22 +0530

    netflix progress
    project progress(working on design)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALSS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
-      
+      <Row 
+       title="NETFLIX ORIGINALS" 
+       fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrendings}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentries}/>
     </div>
   );
 }
